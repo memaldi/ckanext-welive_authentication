30ca68a6966323fac6b314e3f9377ebc9d9aa31b
Mikel Emaldi <m.emaldi@gmail.com>
2015-10-30 10:29:33 +0100

    Updated to new authentication XML

--- ckanext/welive_authentication/plugin.py
+++ ckanext/welive_authentication/plugin.py
@@ -1,13 +1,16 @@
 from ckan.common import request
 from routes.mapper import SubMapper
+import ckan.logic as logic
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import ckan.model as model
 import logging
 import requests
 import xmltodict
 import pylons
 import ConfigParser
 import os
+import uuid
 
 log = logging.getLogger(__name__)
 
@@ -21,10 +24,20 @@ MAIN_SECTION = 'app:main'
 CKAN_SITE_URL = config.get(MAIN_SECTION, 'ckan.site_url')
 
 
+def user_update(context, data_dict=None):
+    return {'success': False, 'message': 'Not allowed to update user profile'}
+
+
 class Welive_AuthenticationPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IAuthenticator)
     plugins.implements(plugins.IRoutes, inherit=True)
+    plugins.implements(plugins.IAuthFunctions)
+
+    # IAuthFunctions
+
+    def get_auth_functions(self):
+        return {'user_update': user_update}
 
     # IConfigurer
 
@@ -38,6 +51,7 @@ class Welive_AuthenticationPlugin(plugins.SingletonPlugin):
     def identify(self):
         log.debug('identify')
         user_name = pylons.session.get('ckanext-welive-user')
+        print user_name
         if user_name:
             # We've found a logged-in user. Set c.user to let CKAN know.
             toolkit.c.user = user_name
@@ -50,26 +64,36 @@ class Welive_AuthenticationPlugin(plugins.SingletonPlugin):
 
             response_dict = xmltodict.parse(response.text)
             username = None
-            if 'serviceResponse' in response_dict:
-                if 'authenticationSuccess' in response_dict['serviceResponse']:
-                    if 'username' in response_dict['serviceResponse']['authenticationSuccess']['attributes']:
-                        username = response_dict['serviceResponse']['authenticationSuccess']['attributes']['username']
-                    elif 'OIDC_CLAIM_email' in response_dict['serviceResponse']['authenticationSuccess']['attributes']:
-                        username = response_dict['serviceResponse']['authenticationSuccess']['attributes']['OIDC_CLAIM_email']
-
+            print response.text
+            print response_dict
+            if 'cas:serviceResponse' in response_dict:
+                if 'cas:authenticationSuccess' in response_dict['cas:serviceResponse']:
+                    if 'cas:username' in response_dict['cas:serviceResponse']['cas:authenticationSuccess']['cas:attributes']:
+                        print 'hey'
+                        username = response_dict['cas:serviceResponse']['cas:authenticationSuccess']['cas:attributes']['cas:username']
+                        print username
+                    elif 'OIDC_CLAIM_email' in response_dict['cas:serviceResponse']['cas:authenticationSuccess']['cas:attributes']:
+                        username = response_dict['cas:serviceResponse']['cas:authenticationSuccess']['cas:attributes']['OIDC_CLAIM_email']
+            print username
             if username is not None:
-                user = toolkit.get_action('user_show')(
-                    context={},
-                    data_dict={'id': username})
-                if user is not None:
-                    toolkit.c.user = user
-                else:
-                    user = toolkit.get_action('user_create')(
-                        context={'ignore_auth': True},
-                        data_dict={'name': username})
-                toolkit.c.user = user['name']
-                pylons.session['ckanext-welive-user'] = user['name']
-                pylons.session.save()
+                try:
+                    print username
+                    user = toolkit.get_action('user_show')(
+                        context={},
+                        data_dict={'id': username})
+                    print user
+                    toolkit.c.user = user['name']
+                    pylons.session['ckanext-welive-user'] = user['name']
+                    pylons.session.save()
+                except logic.NotFound:
+                    user = model.User(name=username,
+                                      email=username,
+                                      password=uuid.uuid4())
+                    user.save()
+                    model.repo.commit()
+                    toolkit.c.user = user.name
+                    pylons.session['ckanext-welive-user'] = user.name
+                    pylons.session.save()
 
     def login(self):
         log.debug('login')
